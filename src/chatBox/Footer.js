e495cae164e4197cdbc9cbcfa2f98b489986d94c
Ayush <aayush199813@gmail.com>
2022-09-07 15:12:14 +0530

    Footer

--- src/chatBox/Footer.js
+++ src/chatBox/Footer.js
@@ -1,5 +1,27 @@
-import React from 'react'
+import React from 'react';
 import classes from './Footer.module.css';
+import { ReactComponent as Smiley } from '../svg/smiley.svg';
+import { ReactComponent as Attach } from '../svg/attach.svg';
+import { ReactComponent as Send } from '../svg/send.svg';
 export default function Footer() {
-  return <div className={classes.container}>Footer</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.icons}>
+        <div>
+          <Smiley />
+        </div>
+        <div>
+          <Attach />
+        </div>
+      </div>
+      <div className={classes.message}>
+        <div className={classes.messagep}>
+          <input type="text" placeholder="Type a message" />
+        </div>
+        <div className={classes.icons}>
+          <Send />
+        </div>
+      </div>
+    </div>
+  );
 }
