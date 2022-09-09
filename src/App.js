244d0b939a3da4dffa660964ba4d7a33d8463fc8
Ayush <aayush199813@gmail.com>
2022-09-10 00:59:00 +0530

    Login page UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import ChatNavigation from '../src/components/sidebar/ChatNavigation';
+import React, { useState } from 'react';
 import './App.css';
+import ChatNavigation from '../src/components/sidebar/ChatNavigation';
 import Chat from './components/chatBox/Chat';
+import Login from './components/Login';
 function App() {
+  const [user, setuser] = useState(false);
   return (
     <div className="App">
-      <ChatNavigation />
-      <Chat />
+      {user ? (
+        <React.Fragment>
+          <ChatNavigation />
+          <Chat />
+        </React.Fragment>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
